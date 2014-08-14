b4f445089f1eb72ebaed16ebf2e6078d8b5fb1c9
T.ED <t.erembertdubal@weezevent.com>
2014-08-14 16:41:24 +0200

    * model factory for container Signed-off-by:T.ED <t.erembertdubal@weezevent.com>

--- Weez/Model/ModelFactory.php
+++ Weez/Model/ModelFactory.php
@@ -4,17 +4,24 @@ namespace Weez\Model;
 
 use \Exception;
 
-class ModelFactory {
+class ModelFactory
+{
 
     private $adapter;
+    private $container;
 
-    public function __construct($adapter) {
-        $this->adapter = $adapter;
+    public function __construct($adapter, $container = null)
+    {
+        $this->adapter   = $adapter;
+        $this->container = $container;
     }
 
-    public function get($class_name) {
+    public function get($class_name)
+    {
         if (class_exists($class_name)) {
-            return new $class_name($this->adapter);
+            $class = new $class_name($this->adapter);
+            $class->setContainer($this->container);
+            return $class;
         }
         throw new Exception("Class Not found");
     }
