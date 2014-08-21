4fdc0dc5b5f569ad04f39cb1b7133b2f2678c106
Julien Bourdin <jbourdin@gmail.com>
2014-08-21 17:34:43 +0200

    fix indent

--- Weez/ZendModelGenerator/Lib/ZendCode/EntityManager.php
+++ Weez/ZendModelGenerator/Lib/ZendCode/EntityManager.php
@@ -246,16 +246,16 @@ class EntityManager extends AbstractGenerator
     {
         $constructBody = '' . PHP_EOL;
         $constructBody .= 'if (! $entity instanceof ' . $this->data['_className'] . 'Entity ){' . PHP_EOL;
-        $constructBody .= '     throw new \Exception(\'Unable to delete: invalid entity\');' . PHP_EOL;
+        $constructBody .= '    throw new \Exception(\'Unable to delete: invalid entity\');' . PHP_EOL;
         $constructBody .= '}' . PHP_EOL;
         $constructBody .= 'if ($useTransaction) {' . PHP_EOL;
-        $constructBody .= '     $this->adapter->getDriver()->getConnection()->beginTransaction();' . PHP_EOL;
+        $constructBody .= '    $this->adapter->getDriver()->getConnection()->beginTransaction();' . PHP_EOL;
         $constructBody .= '}' . PHP_EOL;
         $constructBody .= 'try {' . PHP_EOL;
         if ($this->data['_softDeleteColumn'] != null) {
             foreach ($this->data['_columns'] as $column) {
                 if ($column['field'] == $this->data['_softDeleteColumn']) {
-                    $constructBody .= '     $entity->set' . $column['capital'] . '(';
+                    $constructBody .= '    $entity->set' . $column['capital'] . '(';
                     if ($column['phptype'] == 'boolean') {
                         $constructBody .= 'true';
                     } else {
@@ -265,32 +265,32 @@ class EntityManager extends AbstractGenerator
                     break;
                 }
             }
-            $constructBody .= '     $result = $this->saveEntity($entity,true,false,false);' . PHP_EOL;
+            $constructBody .= '    $result = $this->saveEntity($entity,true,false,false);' . PHP_EOL;
         } else {
             if ($this->data['_primaryKey']['phptype'] == 'array') {
                 $constructBody .= '     $where = array();' . PHP_EOL;
                 foreach ($this->data['_primaryKey']['fields'] as $key) {
-                    $constructBody .= '     $pk_val = $entity->get' . $key['capital'] . '();' . PHP_EOL;
-                    $constructBody .= '         if ($pk_val === null) {' . PHP_EOL;
-                    $constructBody .= '             throw new \Exception(\'The value for ' . $key['capital'] . 'cannot be null\');' . PHP_EOL;
-                    $constructBody .= '         } else {' . PHP_EOL;
-                    $constructBody .= '             $where[] =  array(\'' . $key['field'] . '\' => $pk_val); ' . PHP_EOL;
-                    $constructBody .= '         }' . PHP_EOL;
+                    $constructBody .= '    $pk_val = $entity->get' . $key['capital'] . '();' . PHP_EOL;
+                    $constructBody .= '        if ($pk_val === null) {' . PHP_EOL;
+                    $constructBody .= '            throw new \Exception(\'The value for ' . $key['capital'] . 'cannot be null\');' . PHP_EOL;
+                    $constructBody .= '        } else {' . PHP_EOL;
+                    $constructBody .= '            $where[] =  array(\'' . $key['field'] . '\' => $pk_val); ' . PHP_EOL;
+                    $constructBody .= '        }' . PHP_EOL;
                 }
             } else {
-                $constructBody .= '     $where = array(\'' . $this->data['_primaryKey']['field'] . '\' => $entity->get' . $this->data['_primaryKey']['capital'] . '());' . PHP_EOL;
+                $constructBody .= '    $where = array(\'' . $this->data['_primaryKey']['field'] . '\' => $entity->get' . $this->data['_primaryKey']['capital'] . '());' . PHP_EOL;
             }
-            $constructBody .= '     $result = $this->delete($where);' . PHP_EOL;
+            $constructBody .= '    $result = $this->delete($where);' . PHP_EOL;
         }
 
-        $constructBody .= '     if ($useTransaction) {' . PHP_EOL;
-        $constructBody .= '         $this->adapter->getDriver()->getConnection()->commit();' . PHP_EOL;
-        $constructBody .= '     }' . PHP_EOL;
+        $constructBody .= '    if ($useTransaction) {' . PHP_EOL;
+        $constructBody .= '        $this->adapter->getDriver()->getConnection()->commit();' . PHP_EOL;
+        $constructBody .= '    }' . PHP_EOL;
         $constructBody .= '} catch (Exception $e) {' . PHP_EOL;
-        $constructBody .= '     if ($useTransaction) {' . PHP_EOL;
-        $constructBody .= '         $this->adapter->getDriver()->getConnection()->rollback();' . PHP_EOL;
-        $constructBody .= '     }' . PHP_EOL;
-        $constructBody .= '     $result = false;' . PHP_EOL;
+        $constructBody .= '    if ($useTransaction) {' . PHP_EOL;
+        $constructBody .= '        $this->adapter->getDriver()->getConnection()->rollback();' . PHP_EOL;
+        $constructBody .= '    }' . PHP_EOL;
+        $constructBody .= '    $result = false;' . PHP_EOL;
         $constructBody .= '}' . PHP_EOL;
         $constructBody .= 'return $result;' . PHP_EOL;
         $constructBody .= '' . PHP_EOL;
@@ -313,7 +313,7 @@ class EntityManager extends AbstractGenerator
                         'shortDescription' => 'Deletes the current entity',
                         'longDescription'  => null,
                         'tags'             => array(
-                            new ParamTag('entity', array('Entity')),
+                            new ParamTag('entity', array('Entity'),'Entity to delete'),
                             new ParamTag('useTransaction', array('boolean'), 'Flag to indicate if delete should be done inside a database transaction'),
                             new ReturnTag(array('datatype' => 'Inserted id|false')),
                         )
@@ -328,74 +328,74 @@ class EntityManager extends AbstractGenerator
         $constructBody = '' . PHP_EOL;
         $constructBody .= '$data = $entity->toArray();' . PHP_EOL;
         $constructBody .= 'if ($ignoreEmptyValues) {' . PHP_EOL;
-        $constructBody .= '     foreach ($data as $key => $value) {' . PHP_EOL;
-        $constructBody .= '         if ($value === null or $value === \'\') {' . PHP_EOL;
-        $constructBody .= '             unset($data[$key]);' . PHP_EOL;
-        $constructBody .= '         }' . PHP_EOL;
-        $constructBody .= '     }' . PHP_EOL;
+        $constructBody .= '    foreach ($data as $key => $value) {' . PHP_EOL;
+        $constructBody .= '        if ($value === null or $value === \'\') {' . PHP_EOL;
+        $constructBody .= '            unset($data[$key]);' . PHP_EOL;
+        $constructBody .= '        }' . PHP_EOL;
+        $constructBody .= '    }' . PHP_EOL;
         $constructBody .= '}' . PHP_EOL;
         if ($this->data['_primaryKey']['phptype'] == 'array') {
             $constructBody .= '$primary_key = array();' . PHP_EOL;
             foreach ($this->data['_primaryKey']['fields'] as $key) {
                 $constructBody .= '$pk_val = $entity->get' . $key['capital'] . '();' . PHP_EOL;
                 $constructBody .= 'if ($pk_val === null) {' . PHP_EOL;
-                $constructBody .= '     return false;' . PHP_EOL;
+                $constructBody .= '    return false;' . PHP_EOL;
                 $constructBody .= '} else {' . PHP_EOL;
-                $constructBody .= '     $primary_key[\'' . $key['field'] . '\'] =  $pk_val;' . PHP_EOL;
+                $constructBody .= '    $primary_key[\'' . $key['field'] . '\'] =  $pk_val;' . PHP_EOL;
                 $constructBody .= '}' . PHP_EOL;
             }
             $constructBody .= '$exists = $this->find($primary_key);' . PHP_EOL;
             $constructBody .= '$success = true;' . PHP_EOL;
             $constructBody .= 'if ($useTransaction) {' . PHP_EOL;
-            $constructBody .= '     $this->adapter->getDriver()->getConnection()->beginTransaction();' . PHP_EOL;
+            $constructBody .= '    $this->adapter->getDriver()->getConnection()->beginTransaction();' . PHP_EOL;
             $constructBody .= '}' . PHP_EOL;
             $constructBody .= 'try {' . PHP_EOL;
-            $constructBody .= '     // Check for current existence to know if needs to be inserted' . PHP_EOL;
-            $constructBody .= '     if ($exists === null) {' . PHP_EOL;
-            $constructBody .= '         $this->insert($data);' . PHP_EOL;
+            $constructBody .= '    // Check for current existence to know if needs to be inserted' . PHP_EOL;
+            $constructBody .= '    if ($exists === null) {' . PHP_EOL;
+            $constructBody .= '        $this->insert($data);' . PHP_EOL;
         } else {
             $constructBody .= '$primary_key = $entity->get' . $this->data['_primaryKey']['capital'] . '();' . PHP_EOL;
             $constructBody .= '$success = true;' . PHP_EOL;
             $constructBody .= 'if ($useTransaction) {' . PHP_EOL;
-            $constructBody .= '     $this->adapter->getDriver()->getConnection()->beginTransaction();' . PHP_EOL;
+            $constructBody .= '    $this->adapter->getDriver()->getConnection()->beginTransaction();' . PHP_EOL;
             $constructBody .= '}' . PHP_EOL;
             if (!$this->data['_primaryKey']['foreign_key']) {
                 $constructBody .= 'unset($data[\'' . $this->data['_primaryKey']['field'] . '\']);' . PHP_EOL;
                 $constructBody .= 'try {' . PHP_EOL;
-                $constructBody .= '     if ($primary_key === null) {' . PHP_EOL;
+                $constructBody .= '    if ($primary_key === null) {' . PHP_EOL;
             } else {
                 $constructBody .= '$exists = $this->find($primary_key);' . PHP_EOL;
                 $constructBody .= 'try {' . PHP_EOL;
-                $constructBody .= '     if ($exists === null) {' . PHP_EOL;
+                $constructBody .= '    if ($exists === null) {' . PHP_EOL;
             }
-            $constructBody .= '         $this->insert($data);' . PHP_EOL;
-            $constructBody .= '         $primary_key = $this->getLastInsertValue();' . PHP_EOL;
-            $constructBody .= '         if ($primary_key) {' . PHP_EOL;
-            $constructBody .= '             $entity->set' . $this->data['_primaryKey']['capital'] . '($primary_key);' . PHP_EOL;
+            $constructBody .= '        $this->insert($data);' . PHP_EOL;
+            $constructBody .= '        $primary_key = $this->getLastInsertValue();' . PHP_EOL;
+            $constructBody .= '        if ($primary_key) {' . PHP_EOL;
+            $constructBody .= '            $entity->set' . $this->data['_primaryKey']['capital'] . '($primary_key);' . PHP_EOL;
             if ($this->data['_returnId']) {
-                $constructBody .= '         $success = $primary_key;' . PHP_EOL;
+                $constructBody .= '        $success = $primary_key;' . PHP_EOL;
             }
-            $constructBody .= '         } else {' . PHP_EOL;
-            $constructBody .= '             $success = false;' . PHP_EOL;
-            $constructBody .= '         }' . PHP_EOL;
+            $constructBody .= '        } else {' . PHP_EOL;
+            $constructBody .= '            $success = false;' . PHP_EOL;
+            $constructBody .= '        }' . PHP_EOL;
         }
-        $constructBody .= '     } else {' . PHP_EOL;
-        $constructBody .= '         $this->update($data,array(' . PHP_EOL;
+        $constructBody .= '    } else {' . PHP_EOL;
+        $constructBody .= '        $this->update($data,array(' . PHP_EOL;
         if ($this->data['_primaryKey']['phptype'] == 'array') {
             $fields = count($this->data['_primaryKey']['fields']);
             foreach ($this->data['_primaryKey']['fields'] as $key) {
-                $constructBody .= '             \'' . $key['field'] . ' = ?\' => $primary_key[\'' . $key['field'] . '\'],' . PHP_EOL;
+                $constructBody .= '            \'' . $key['field'] . ' = ?\' => $primary_key[\'' . $key['field'] . '\'],' . PHP_EOL;
             }
         } else {
-            $constructBody .='              \'' . $this->data['_primaryKey']['field'] . ' = ?\' => $primary_key' . PHP_EOL;
+            $constructBody .='             \'' . $this->data['_primaryKey']['field'] . ' = ?\' => $primary_key' . PHP_EOL;
         }
-        $constructBody .= '             )' . PHP_EOL;
-        $constructBody .= '         );' . PHP_EOL;
-        $constructBody .= '     }' . PHP_EOL;
+        $constructBody .= '            )' . PHP_EOL;
+        $constructBody .= '        );' . PHP_EOL;
+        $constructBody .= '    }' . PHP_EOL;
         if (count($this->data['dependentTables']) > 0) {
-            $constructBody .= '     if ($recursive) {' . PHP_EOL;
+            $constructBody .= '    if ($recursive) {' . PHP_EOL;
             foreach ($this->data['dependentTables'] as $key) {
-                $constructBody .= '         if ($success && $entity->get' . $this->data['relationNameDependent'][$key['key_name']] . '() !== null) {' . PHP_EOL;
+                $constructBody .= '        if ($success && $entity->get' . $this->data['relationNameDependent'][$key['key_name']] . '() !== null) {' . PHP_EOL;
                 if ($key['type'] !== 'many') {
                     $constructBody .= '$success = $success &&  $entity->get' . $this->data['relationNameDependent'][$key['key_name']] . '()' . PHP_EOL;
                     if ($this->data['_primaryKey']['phptype'] !== 'array') {
@@ -403,37 +403,37 @@ class EntityManager extends AbstractGenerator
                     }
                     $constructBody .= '->save($ignoreEmptyValues, $recursive, false);' . PHP_EOL;
                 } else {
-                    $constructBody .= '             $' . $this->data['classNameDependent'][$key['key_name']]['foreign_tbl_name'] . ' = $entity->get' . $this->data['relationNameDependent'][$key['key_name']] . '();' . PHP_EOL;
-                    $constructBody .= '             foreach ($' . $this->data['classNameDependent'][$key['key_name']]['foreign_tbl_name'] . ' as $value) {' . PHP_EOL;
-                    $constructBody .= '                 $success = $success && $value' . PHP_EOL;
+                    $constructBody .= '            $' . $this->data['classNameDependent'][$key['key_name']]['foreign_tbl_name'] . ' = $entity->get' . $this->data['relationNameDependent'][$key['key_name']] . '();' . PHP_EOL;
+                    $constructBody .= '            foreach ($' . $this->data['classNameDependent'][$key['key_name']]['foreign_tbl_name'] . ' as $value) {' . PHP_EOL;
+                    $constructBody .= '                $success = $success && $value' . PHP_EOL;
                     if ($this->data['_primaryKey']['phptype'] !== 'array') {
-                        $constructBody .= '                     ->set' . $this->_getCapital($key['column_name']) . '($primary_key)' . PHP_EOL;
+                        $constructBody .= '                    ->set' . $this->_getCapital($key['column_name']) . '($primary_key)' . PHP_EOL;
                     } elseif (is_array($key['column_name'])) {
                         foreach (explode(',', $key['column_name'][0]) as $_column) {
                             $column = trim(str_replace('`', '', $_column));
-                            $constructBody .= '                 ->set' . $this->_getCapital($column) . '($primary_key[\'' . $column . '\'])' . PHP_EOL;
+                            $constructBody .= '                ->set' . $this->_getCapital($column) . '($primary_key[\'' . $column . '\'])' . PHP_EOL;
                         }
                     }
-                    $constructBody .= '                 ->save($ignoreEmptyValues, $recursive, false);' . PHP_EOL;
-                    $constructBody .= '                 if (! $success) {' . PHP_EOL;
-                    $constructBody .= '                     break;' . PHP_EOL;
-                    $constructBody .= '                 }' . PHP_EOL;
-                    $constructBody .= '             }' . PHP_EOL;
+                    $constructBody .= '                ->save($ignoreEmptyValues, $recursive, false);' . PHP_EOL;
+                    $constructBody .= '                if (! $success) {' . PHP_EOL;
+                    $constructBody .= '                    break;' . PHP_EOL;
+                    $constructBody .= '                }' . PHP_EOL;
+                    $constructBody .= '            }' . PHP_EOL;
                 }
-                $constructBody .= '         }' . PHP_EOL;
+                $constructBody .= '        }' . PHP_EOL;
             }
-            $constructBody .= '     }' . PHP_EOL;
+            $constructBody .= '    }' . PHP_EOL;
         }
-        $constructBody .= '     if ($useTransaction && $success) {' . PHP_EOL;
-        $constructBody .= '         $this->adapter->getDriver()->getConnection()->commit();' . PHP_EOL;
-        $constructBody .= '     } elseif ($useTransaction) {' . PHP_EOL;
-        $constructBody .= '         $this->adapter->getDriver()->getConnection()->rollback();' . PHP_EOL;
-        $constructBody .= '     }' . PHP_EOL;
+        $constructBody .= '    if ($useTransaction && $success) {' . PHP_EOL;
+        $constructBody .= '        $this->adapter->getDriver()->getConnection()->commit();' . PHP_EOL;
+        $constructBody .= '    } elseif ($useTransaction) {' . PHP_EOL;
+        $constructBody .= '        $this->adapter->getDriver()->getConnection()->rollback();' . PHP_EOL;
+        $constructBody .= '    }' . PHP_EOL;
         $constructBody .= '} catch (Exception $e) {' . PHP_EOL;
-        $constructBody .= '     if ($useTransaction) {' . PHP_EOL;
-        $constructBody .= '         $this->adapter->getDriver()->getConnection()->rollback();' . PHP_EOL;
-        $constructBody .= '     }' . PHP_EOL;
-        $constructBody .= '     $success = false;' . PHP_EOL;
+        $constructBody .= '    if ($useTransaction) {' . PHP_EOL;
+        $constructBody .= '        $this->adapter->getDriver()->getConnection()->rollback();' . PHP_EOL;
+        $constructBody .= '    }' . PHP_EOL;
+        $constructBody .= '    $success = false;' . PHP_EOL;
         $constructBody .= '}' . PHP_EOL;
         $constructBody .= 'return $success;' . PHP_EOL;
         $methods[] = array(
@@ -463,7 +463,7 @@ class EntityManager extends AbstractGenerator
                         'shortDescription' => 'Saves current row, and optionally dependent rows',
                         'longDescription'  => null,
                         'tags'             => array(
-                            new ParamTag('entity', array('Entity')),
+                            new ParamTag('entity', array('Entity'),'Entity to save'),
                             new ParamTag('ignoreEmptyValues', array('boolean'), 'Should empty values saved'),
                             new ParamTag('recursive', array('boolean'), 'Should the object graph be walked for all related elements'),
                             new ParamTag('useTransaction', array('boolean'), 'Flag to indicate if save should be done inside a database transaction'),
